adeade805010adb809e0d496e4b629463747e1f5
Jiucheng Ding <jd2263@cbsumm39.biohpc.cornell.edu>
2023-04-28 19:17:49 -0400

    Finally, actually fixed shapes and color of PCoA. The colors were flipped before.

--- code/colors_and_shapes.R
+++ code/colors_and_shapes.R
@@ -2,27 +2,28 @@
 # let's keep the visualizations consistent! :) 
 
 location_shapes <- c(
-  "FC" = 23, 
-  "JJ" = 21)
+  "FC" = 16, 
+  "JJ" = 17)
 
-#location_colors <- c(
- # "FC" = "goldenrod1", 
- # "JJ" = "darkorange2")
+location_colors <- c(
+  "FC" = "goldenrod1", 
+  "JJ" = "darkorange2")
 
 
 heat_colors <- c(
-  "hot" = "steelblue3",
-  "cool" = "firebrick3")
+  "hot" = "firebrick1",
+  "cool" = "deepskyblue")
 
-#heat_shapes <- c(
-#  "hot" = "14",
- # "cool" = "15")
+heat_shapes <- c(
+ "hot" = "14",
+ "cool" = "15")
+
 # Set the ggplot theme
 theme_set(theme_bw() + 
             theme(axis.title = element_text(size = 12),
                   axis.text = element_text(size = 10),
                   legend.title = element_text(size = 10),
-                  legend.text = element_text(size = 9)))
+                  legend.text = element_text(size = 8)))
 
 # Set the phylum colors
 phylum_colors <- c(
